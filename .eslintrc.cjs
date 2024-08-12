f8ac93b477cef56f71669d14567a3beb8b8ec2fb
Chaosborne <rick.chaosborne@gmail.com>
2024-08-12 22:09:31 +0300

    Create project

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+// module.exports = {
+//   root: true,
+//   env: { browser: true, es2020: true },
+//   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react-hooks/recommended"],
+//   ignorePatterns: ["dist", ".eslintrc.cjs"],
+//   parser: "@typescript-eslint/parser",
+//   plugins: ["react-refresh"],
+//   rules: {
+//     "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+//   },
+// };
+
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json', './tsconfig.app.json'],
+    tsconfigRootDir: __dirname,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended-type-checked', 'plugin:react/recommended', 'plugin:react/jsx-runtime', 'plugin:react-hooks/recommended', 'prettier'],
+  plugins: ['react-refresh', 'react', 'prettier'],
+  settings: {
+    react: {
+      version: 'detect', // Автоматически определяет версию React
+    },
+  },
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'prettier/prettier': 'error',
+  },
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+};
