65d49dcbaca58d3aeadc7ee3bbaeaf0074f23cea
Chaosborne <rick.chaosborne@gmail.com>
2024-08-13 21:43:30 +0300

    App header and categories

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,43 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './scss/App.scss';
 
+
 function App() {
-  const [count, setCount] = useState(0);
+  const categoryFiller = (
+    <div className="app-main__category"><svg width='231'><rect x="0" y="0" width='231' height='214' fill="#808080"/></svg></div>
+  )
+
+  const searchSubmitHandler = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    console.log('searchSubmitHandler works')
+  }
 
   return (
     <>
-      <div className="container">
-        <div>
-          <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank" rel="noreferrer">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
+      <header className="app-header">
+        <div className="container">
+          <div className="app-header__inner">
+            <div className="app-header__logo">App header logo</div>
+            <form className="app-header__search" onSubmit={searchSubmitHandler}>
+              <input className="app-header__search-input" type="text" />
+              <button className="app-header__search-btn" type="submit">
+                lens img to be here
+              </button>
+            </form>
+            <a href="#" className="app-header__login">Войти</a>
+            <a href="#" className="app-header__favourites">Избранное</a>
+            <a href="#" className="app-header__cart">Корзина</a>
+          </div>
         </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
+      </header>
+
+      <main className="app-main">
+        <div className="container">
+          <h2 className="app-main__title">Категории</h2>
+          <div className="app-main__categories">
+            {categoryFiller}{categoryFiller}{categoryFiller}{categoryFiller}{categoryFiller}
+          </div>
         </div>
-        <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-      </div>
+      </main>
     </>
   );
 }
