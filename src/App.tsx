d674581936dfc8bf260bd0639e785fc342cf54ec
Chaosborne <rick.chaosborne@gmail.com>
2024-08-14 16:16:36 +0300

    More layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import './scss/App.scss';
 
 function App() {
   const categoryFiller = (
-    <div className="app-main__category"><svg width='231'><rect x="0" y="0" width='231' height='214' fill="#808080"/></svg></div>
+    <svg width='244' height='200'><rect x="0" y="0" width='244' height='200' fill="#808080"/></svg>
+  )
+  const productFiller = (
+    <svg width='249' height='320'><rect x="0" y="0" width='249' height='320' fill="#808080"/></svg>
   )
 
   const searchSubmitHandler = (e: { preventDefault: () => void; }) => {
@@ -31,12 +34,55 @@ function App() {
       </header>
 
       <main className="app-main">
-        <div className="container">
-          <h2 className="app-main__title">Категории</h2>
-          <div className="app-main__categories">
-            {categoryFiller}{categoryFiller}{categoryFiller}{categoryFiller}{categoryFiller}
+        
+        <section className="categories">
+          <div className="container">
+            <h2 className="categories__title">Категории</h2>
+            <div className="categories__items">
+              {categoryFiller}{categoryFiller}{categoryFiller}{categoryFiller}{categoryFiller}
+            </div>
           </div>
-        </div>
+        </section>
+
+        <section className="store">
+          <div className="container">
+            <div className="store__controls">
+              <div className="price-sort">Price sort</div>
+              <div className="list-tile-toggler">List-tile toggler</div>
+            </div>
+            <aside className="filter-and-products">
+              <div className="filter">
+                <div className="filter__title">Filter</div>
+                <form action="" className="filter__form">
+                  <div className="filter__select">
+                    <input type="checkbox" name="apple" id="apple"  />
+                    <a href="#">Apple</a>
+                  </div>
+                  <div className="filter__select">
+                    <input type="checkbox" name="samsung" id="samsung"  />
+                    <a href="#">Samsung</a>
+                  </div>
+                  <div className="filter__select">
+                    <input type="checkbox" name="xiaomi" id="xiaomi"  />
+                    <a href="#">Xiaomi</a>
+                  </div>
+                  <div className="filter__select">
+                    <input type="checkbox" name="realme" id="realme"  />
+                    <a href="#">Realme</a>
+                  </div>
+                  <div className="filter__select">
+                    <input type="checkbox" name="oppo" id="oppo"  />
+                    <a href="#">Oppo</a>
+                  </div>
+                </form>
+              </div>
+              <div className="products">
+                {productFiller}{productFiller}{productFiller}{productFiller}{productFiller}
+              </div>
+            </aside>
+          </div>
+        </section>
+
       </main>
     </>
   );
