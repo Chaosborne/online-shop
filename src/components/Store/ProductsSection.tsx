243ddc4e263c0b332991e5c3ff382c2ed7b75342
Chaosborne <rick.chaosborne@gmail.com>
2024-08-17 18:45:31 +0300

    ProductsSection

---
+++ src/components/Store/ProductsSection.tsx
@@ -0,0 +1,58 @@
+import styles from './ProductsSection.module.scss';
+
+const ProductSection = () => {
+    const toggleProductsView = () => {
+        const productsList = document.querySelector('.products');
+        productsList && (
+          productsList.classList.toggle('products-tiles'),
+          productsList.classList.toggle('products-lines')
+          );
+      };
+
+    return (
+        <section className={styles['product-section']}>
+            <div className="container">
+                <div className={styles.store__controls}>
+                <div className={styles['price-sort']}>Price sort</div>
+                <div className={styles['list-tile-toggler']} onClick={toggleProductsView}>Toggle list/tile</div>
+                </div>
+                <aside className={styles['filter-and-products']}>
+                <div className={styles.filter}>
+                    <div className={styles.filter__title}>Filter</div>
+                    <form className={styles.filter__form} action="" >
+                    <div className={styles.filter__select}>
+                        <input type="checkbox" name="apple" id="apple"  />
+                        <a href="#">Apple</a>
+                    </div>
+                    <div className={styles.filter__select}>
+                        <input type="checkbox" name="samsung" id="samsung"  />
+                        <a href="#">Samsung</a>
+                    </div>
+                    <div className={styles.filter__select}>
+                        <input type="checkbox" name="xiaomi" id="xiaomi"  />
+                        <a href="#">Xiaomi</a>
+                    </div>
+                    <div className={styles.filter__select}>
+                        <input type="checkbox" name="realme" id="realme"  />
+                        <a href="#">Realme</a>
+                    </div>
+                    <div className={styles.filter__select}>
+                        <input type="checkbox" name="oppo" id="oppo"  />
+                        <a href="#">Oppo</a>
+                    </div>
+                    </form>
+                </div>
+                <div className={`${styles.products} ${styles['products-tiles']}`}>
+                    <div className={styles.card}></div>
+                    <div className={styles.card}></div>
+                    <div className={styles.card}></div>
+                    <div className={styles.card}></div>
+                    <div className={styles.card}></div>
+                </div>
+                </aside>
+            </div>
+        </section>
+    )
+}
+
+export default ProductSection;
--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,63 +1,15 @@
 import styles from "./Main.module.scss";
 import Categories from "../Categories/Categories";
+import ProductsSection from "../Store/ProductsSection";
 
 const Main = () => {
 
-    const toggleProductsView = () => {
-        const productsList = document.querySelector('.products');
-        productsList && (
-          productsList.classList.toggle('products-tiles'),
-          productsList.classList.toggle('products-lines')
-          );
-      };
+    
 
       return (
         <main className={styles[`app-main`]}>
             <Categories />
-
-            <section className="store">
-            <div className="container">
-                <div className="store__controls">
-                <div className="price-sort">Price sort</div>
-                <div className="list-tile-toggler" onClick={toggleProductsView}>Toggle list/tile</div>
-                </div>
-                <aside className="filter-and-products">
-                <div className="filter">
-                    <div className="filter__title">Filter</div>
-                    <form action="" className="filter__form">
-                    <div className="filter__select">
-                        <input type="checkbox" name="apple" id="apple"  />
-                        <a href="#">Apple</a>
-                    </div>
-                    <div className="filter__select">
-                        <input type="checkbox" name="samsung" id="samsung"  />
-                        <a href="#">Samsung</a>
-                    </div>
-                    <div className="filter__select">
-                        <input type="checkbox" name="xiaomi" id="xiaomi"  />
-                        <a href="#">Xiaomi</a>
-                    </div>
-                    <div className="filter__select">
-                        <input type="checkbox" name="realme" id="realme"  />
-                        <a href="#">Realme</a>
-                    </div>
-                    <div className="filter__select">
-                        <input type="checkbox" name="oppo" id="oppo"  />
-                        <a href="#">Oppo</a>
-                    </div>
-                    </form>
-                </div>
-                <div className="products products-tiles">
-                    <div className="card"></div>
-                    <div className="card"></div>
-                    <div className="card"></div>
-                    <div className="card"></div>
-                    <div className="card"></div>
-                </div>
-                </aside>
-            </div>
-            </section>
-
+            <ProductsSection />
         </main>
       )
 
