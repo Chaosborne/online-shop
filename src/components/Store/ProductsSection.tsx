ba4086be0d1cae7253e959ccc99805fbfbf8043d
Chaosborne <rick.chaosborne@gmail.com>
2024-09-20 15:11:02 +0300

    products with properties map

--- src/components/Store/ProductsSection.tsx
+++ src/components/Store/ProductsSection.tsx
@@ -1,6 +1,52 @@
 import styles from './ProductsSection.module.scss';
 import { useState } from 'react';
 
+const PRODUCTS = [
+  {
+    id: 'item1',
+    itemCategoty: 'someCategory',
+    itemImg: 'img',
+    itemName: 'Название',
+    itemDescription: 'Описание',
+    itemPrice: 'Цена',
+  },
+  {
+    id: 'item2',
+    itemCategoty: 'someCategory',
+    itemImg: 'img 2',
+    itemName: 'Название 2',
+    itemDescription: 'Описание 2',
+    itemPrice: 'Цена 2',
+  },
+  {
+    id: 'item3',
+    itemCategoty: 'someCategory',
+    itemImg: 'img 3',
+    itemName: 'Название 3',
+    itemDescription: 'Описание 3',
+    itemPrice: 'Цена 3',
+  },
+  {
+    id: 'item4',
+    itemCategoty: 'someCategory',
+    itemImg: 'img 4',
+    itemName: 'Название 4',
+    itemDescription: 'Описание 4',
+    itemPrice: 'Цена 4',
+  },
+];
+
+const productsList = PRODUCTS.map(item => {
+  return (
+    <div className={styles.card} key={item.id}>
+      <div>{item.itemImg}</div>
+      <div>{item.itemName}</div>
+      <div>{item.itemDescription}</div>
+      <div>{item.itemPrice}</div>
+    </div>
+  );
+});
+
 const ProductSection = () => {
   const [isTilesView, setIsTilesView] = useState(true);
   const toggleProductsView = () => setIsTilesView(!isTilesView);
@@ -40,13 +86,7 @@ const ProductSection = () => {
               </div>
             </form>
           </div>
-          <div className={`${styles.products} ${isTilesView ? styles['products-tiles'] : styles['products-lines']}`}>
-            <div className={styles.card}></div>
-            <div className={styles.card}></div>
-            <div className={styles.card}></div>
-            <div className={styles.card}></div>
-            <div className={styles.card}></div>
-          </div>
+          <div className={`${styles.products} ${isTilesView ? styles['products-tiles'] : styles['products-lines']}`}>{productsList}</div>
         </aside>
       </div>
     </section>
