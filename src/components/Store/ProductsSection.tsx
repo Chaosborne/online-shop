118203f04f0b414624ce2ecda1de7872a6726a19
Chaosborne <rick.chaosborne@gmail.com>
2024-09-26 01:16:34 +0300

    sort by brand

--- src/components/Store/ProductsSection.tsx
+++ src/components/Store/ProductsSection.tsx
@@ -7,6 +7,7 @@ const PRODUCTS = [
     id: 'item1',
     itemCategoty: 'someCategory',
     itemImg: 'img',
+    itemBrand: 'Apple',
     itemName: 'Название',
     itemDescription: 'Описание',
     itemPrice: 100,
@@ -15,6 +16,7 @@ const PRODUCTS = [
     id: 'item2',
     itemCategoty: 'someCategory',
     itemImg: 'img 2',
+    itemBrand: 'Apple',
     itemName: 'Название 2',
     itemDescription: 'Описание 2',
     itemPrice: 200,
@@ -23,6 +25,7 @@ const PRODUCTS = [
     id: 'item3',
     itemCategoty: 'someCategory',
     itemImg: 'img 3',
+    itemBrand: 'Samsung',
     itemName: 'Название 3',
     itemDescription: 'Описание 3',
     itemPrice: 300,
@@ -31,6 +34,7 @@ const PRODUCTS = [
     id: 'item4',
     itemCategoty: 'someCategory',
     itemImg: 'img 4',
+    itemBrand: 'Realme',
     itemName: 'Название 4',
     itemDescription: 'Описание 4',
     itemPrice: 400,
@@ -40,16 +44,30 @@ const PRODUCTS = [
 
 const ProductSection = () => {
   const [isTilesView, setIsTilesView] = useState(true);
-  const toggleProductsView = () => setIsTilesView(!isTilesView);
-
+  const [selectedBrands, setSelectedBrands] = useState<string[]>([]);
   const [isAscending, setIsAscending] = useState(true);
+
+  const toggleProductsView = () => setIsTilesView(!isTilesView);
   const togglePriceSort = () => setIsAscending(!isAscending);
 
-  const productsList = PRODUCTS.sort((a, b) => (isAscending ? a.itemPrice - b.itemPrice : b.itemPrice - a.itemPrice)).map(item => {
+  const handleBrandCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, checked } = e.target;
+
+    if (checked) {
+      setSelectedBrands(prevSelectedBrands => [...prevSelectedBrands, name]);
+    } else {
+      setSelectedBrands(prevSelectedBrands => prevSelectedBrands.filter(brand => brand !== name));
+    }
+  };
+
+  const filteredProducts = PRODUCTS.filter(product => (selectedBrands.length > 0 ? selectedBrands.includes(product.itemBrand.toLowerCase()) : true)).sort((a, b) => (isAscending ? a.itemPrice - b.itemPrice : b.itemPrice - a.itemPrice));
+
+  const productsList = filteredProducts.map(item => {
     return (
       <div className={styles.card} key={item.id}>
         <div>{item.itemImg}</div>
         <div>{item.itemName}</div>
+        <div>{item.itemBrand}</div>
         <div>{item.itemDescription}</div>
         <div>{item.itemPrice}</div>
       </div>
@@ -72,23 +90,23 @@ const ProductSection = () => {
             <div className={styles.filter__title}>Filter</div>
             <form className={styles.filter__form} action="">
               <div className={styles.filter__select}>
-                <input type="checkbox" name="apple" id="apple" />
+                <input type="checkbox" name="apple" id="apple" onChange={handleBrandCheckboxChange} />
                 <label htmlFor="apple">Apple</label>
               </div>
               <div className={styles.filter__select}>
-                <input type="checkbox" name="samsung" id="samsung" />
+                <input type="checkbox" name="samsung" id="samsung" onChange={handleBrandCheckboxChange} />
                 <label htmlFor="samsung">Samsung</label>
               </div>
               <div className={styles.filter__select}>
-                <input type="checkbox" name="xiaomi" id="xiaomi" />
+                <input type="checkbox" name="xiaomi" id="xiaomi" onChange={handleBrandCheckboxChange} />
                 <label htmlFor="xiaomi">Xiaomi</label>
               </div>
               <div className={styles.filter__select}>
-                <input type="checkbox" name="realme" id="realme" />
+                <input type="checkbox" name="realme" id="realme" onChange={handleBrandCheckboxChange} />
                 <label htmlFor="realme">Realme</label>
               </div>
               <div className={styles.filter__select}>
-                <input type="checkbox" name="oppo" id="oppo" />
+                <input type="checkbox" name="oppo" id="oppo" onChange={handleBrandCheckboxChange} />
                 <label htmlFor="oppo">Oppo</label>
               </div>
             </form>
