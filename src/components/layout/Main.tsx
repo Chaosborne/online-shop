99d9519a78d7c2e3dc01ba9bd129d6823e7389d0
Chaosborne <rick.chaosborne@gmail.com>
2024-10-29 14:43:35 +0300

    linter

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -2,7 +2,7 @@ import styles from './Main.module.scss';
 import Categories from '../Categories/Categories';
 import ProductsSection from '../Store/ProductsSection';
 
-const Main = ({ products }: { products: { id: string; itemCategoty: string; itemImg: string; itemBrand: string; itemName: string; itemDescription: string; itemPrice: number }[] }) => { // Описываем объект и сообщаем, что этот объект повторяется в массиве
+const Main = ({ products }: { products: { id: string; itemCategoty: string; itemImg: string; itemBrand: string; itemName: string; itemDescription: string; itemPrice: number }[] }) => {
   return (
     <main className={styles[`app-main`]}>
       <Categories />
