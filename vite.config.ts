0003fd5a3eb18dd264c09915fbd78093073e60ee
Chaosborne <rick.chaosborne@gmail.com>
2024-11-12 19:54:34 +0300

    Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'public/index.html'),
+        app: resolve(__dirname, 'app.html'),
+      },
+      output: {
+        entryFileNames: chunkInfo => {
+          return chunkInfo.name === 'main' ? '[name].js' : 'app/[name].js';
+        },
+        assetFileNames: chunkInfo => {
+          return chunkInfo.name === 'main' ? 'css/[name][extname]' : 'app/[name][extname]';
+        },
+      },
+    },
+  },
+});
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
