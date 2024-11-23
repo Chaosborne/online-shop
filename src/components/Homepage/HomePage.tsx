b053d268c78517cc988699be14fb02762f87d062
Chaosborne <rick.chaosborne@gmail.com>
2024-11-23 21:31:37 +0300

    scss refactoring

--- src/components/Homepage/HomePage.tsx
+++ src/components/Homepage/HomePage.tsx
@@ -1,11 +1,12 @@
+import styles from './HomePage.module.scss';
 import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   return (
     <header>
-      <div className="header-menu">
+      <div className={styles['header-menu']}>
         <div className="container">
-          <div className="header-menu__inner">
+          <div className={styles['header-menu__inner']}>
             <h1>Это главная страница</h1>
             <Link to={'/shop'}>Перейти в магазин</Link>
           </div>
