91e7c5818307cf45ec19e0d5172806406c422ece
Chaosborne <rick.chaosborne@gmail.com>
2024-11-28 23:13:53 +0300

    eslintrc ignore sass.config.js

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,3 @@
-// module.exports = {
-//   root: true,
-//   env: { browser: true, es2020: true },
-//   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react-hooks/recommended"],
-//   ignorePatterns: ["dist", ".eslintrc.cjs"],
-//   parser: "@typescript-eslint/parser",
-//   plugins: ["react-refresh"],
-//   rules: {
-//     "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
-//   },
-// };
-
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
@@ -31,5 +19,5 @@ module.exports = {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     'prettier/prettier': 'error',
   },
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'sass.config.js'],
 };
