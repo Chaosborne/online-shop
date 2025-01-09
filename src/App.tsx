839ae07ebe0248dde7490f158d3703eee0a55eea
Chaosborne <rick.chaosborne@gmail.com>
2025-01-09 18:43:45 +0300

    some refactoring in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,10 @@
-// import './App.scss';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import { DefaultLayout } from './components/layouts';
-// import { routes } from './constants/routes';
-
-// function App() {
-//   /////////////// Ignore React Router v7 Future Flag Warning
-//   const originalWarn = console.warn;
-//   console.warn = (...args) => {
-//     if (typeof args[0] === 'string' && args[0].includes('React Router Future Flag Warning')) {
-//       return;
-//     }
-//     originalWarn(...args);
-//   };
-//   ///////////////
-
-//   return (
-//     <Router>
-//       <DefaultLayout>
-//         <Routes>
-//           {routes.map(({ path, element }, index) => (
-//             <Route key={index} path={path} element={element} />
-//           ))}
-//         </Routes>
-//       </DefaultLayout>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
 import './App.scss';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { DefaultLayout } from './components/layouts';
 import { routes } from './constants/routes';
+
+/////////////// Work with Firebase products and store
+/////////////// This piece of code to be placed elsewhere to avoid cluttering the App
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchProductsFromFirebase } from './store/slices/getDbProductsSlice';
@@ -41,8 +13,6 @@ import { useSelector } from 'react-redux';
 import { RootState } from './store/store'; // Путь к типам RootState
 
 function App() {
-  /////////////// Work with Firebase products and store
-  /////////////// This piece of code to be placed elsewhere to avoid cluttering the App
   const dispatch = useDispatch<AppDispatch>();
   const productsState = useSelector((state: RootState) => state.dbProducts);
   useEffect(() => {
@@ -50,10 +20,11 @@ function App() {
   }, [dispatch]);
   useEffect(() => {
     if (productsState.products.length > 0) {
-      console.log('Products state from store:', productsState);
+      const productsFromStore = productsState.products;
+      console.log('Products from store:', productsFromStore);
     }
   }, [productsState]);
-  ///////////////
+  /////////////// End of work with Firebase products and store
 
   /////////////// Ignore React Router v7 Future Flag Warning
   const originalWarn = console.warn;
