18312a74d9116d4873ed70d175646855f07977de
Chaosborne <rick.chaosborne@gmail.com>
2025-03-30 20:59:06 +0300

    shop header layout

--- src/components/widgets/ShopHeader/ShopHeader.tsx
+++ src/components/widgets/ShopHeader/ShopHeader.tsx
@@ -112,15 +112,27 @@ const ShopHeader = () => {
             <input id="app-header__search-input" className={s.SearchInput} type="text" onInput={searchSuggestionsHandler} />
             <SearchSuggestions matchingItems={matchingItems} hideWhenClick={hideWhenClick} suggestionsListRef={suggestionsListRef} />
             <button className={s.SearchBtn} type="submit">
-              lens img to be here
+              <svg width="15.000946" height="15.000000" viewBox="0 0 20.0009 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M14.57 16C10.57 19.18 4.98 18.3 2.02 14.65C-0.83 11.12 -0.65 6.04 2.44 2.82C5.65 -0.51 10.68 -0.95 14.33 1.78C15.64 2.76 16.61 4 17.25 5.5C17.89 7.02 18.08 8.59 17.86 10.22C17.64 11.83 17 13.27 15.94 14.62C16.03 14.67 16.12 14.71 16.19 14.78C17.34 15.93 18.49 17.08 19.64 18.23C19.92 18.5 20.05 18.82 19.97 19.2C19.81 19.96 18.91 20.25 18.32 19.73C18.05 19.49 17.8 19.22 17.54 18.96C16.6 18.01 15.65 17.06 14.7 16.12C14.66 16.08 14.62 16.05 14.57 16ZM15.96 8.98C15.97 5.12 12.85 2 8.98 2C5.12 1.99 2 5.09 1.99 8.94C1.97 12.81 5.08 15.94 8.96 15.96C12.81 15.98 15.95 12.85 15.96 8.98Z"
+                  fill="#FFFFFF"
+                  fillOpacity="1.000000"
+                  fillRule="nonzero"
+                />
+              </svg>
             </button>
           </form>
           <nav className={s.AppNav}>
             <ul className={s.AppMenu}>
               <li>
-                <button className={s.clearBtn} onClick={localStorageAndConsoleClearHandler}>
-                  Clear LS & C
-                </button>
+                <Link className={s.Favourites} to="shop/my/Favorites">
+                  Избранное
+                </Link>
+              </li>
+              <li>
+                <Link className={s.Cart} to="/shop/my/Cart">
+                  Корзина <span>{cart.totalQuantity}</span>
+                </Link>
               </li>
               <li>
                 {user ? (
@@ -134,14 +146,9 @@ const ShopHeader = () => {
                 )}
               </li>
               <li>
-                <Link className={s.Favourites} to="shop/my/Favorites">
-                  Избранное
-                </Link>
-              </li>
-              <li>
-                <Link className={s.Cart} to="/shop/my/Cart">
-                  Корзина <span>{cart.totalQuantity}</span>
-                </Link>
+                <button className={s.clearBtn} onClick={localStorageAndConsoleClearHandler}>
+                  Clear LS & C
+                </button>
               </li>
             </ul>
           </nav>
