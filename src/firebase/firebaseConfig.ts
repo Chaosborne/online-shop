2c6460bd5bad90b544bb04e4898d77f2827c9b99
Chaosborne <rick.chaosborne@gmail.com>
2025-06-07 22:14:38 +0300

    migration to Firestore

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,13 +1,12 @@
-// Firebase (Also change in getDBProductsSlice.ts)
+// Firestore (Also change in getDBProductsSlice.ts)
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getDatabase } from 'firebase/database';
 import { firebaseApiKey } from './firebaseApiKey';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: firebaseApiKey,
   authDomain: 'eco-village-d5d6d.firebaseapp.com',
-  databaseURL: 'https://eco-village-d5d6d-default-rtdb.firebaseio.com',
   projectId: 'eco-village-d5d6d',
   storageBucket: 'eco-village-d5d6d.firebasestorage.app',
   messagingSenderId: '544483561624',
@@ -16,27 +15,4 @@ const firebaseConfig = {
 
 export const firebaseApp = initializeApp(firebaseConfig);
 export const auth = getAuth(firebaseApp);
-export const rtdb = getDatabase(firebaseApp);
-
-// Firestore (Also change in getDBProductsSlice.ts)
-// import { initializeApp } from 'firebase/app';
-// import { getAuth } from 'firebase/auth';
-// import { firebaseApiKey } from './firebaseApiKey';
-// import { getFirestore } from 'firebase/firestore';
-
-// const firebaseConfig = {
-//   apiKey: firebaseApiKey,
-//   authDomain: 'eco-village-d5d6d.firebaseapp.com',
-//   projectId: 'eco-village-d5d6d',
-//   storageBucket: 'eco-village-d5d6d.firebasestorage.app',
-//   messagingSenderId: '544483561624',
-//   appId: '1:544483561624:web:d67843478bba766e1180ed',
-// };
-
-// export const firebaseApp = initializeApp(firebaseConfig);
-
-// export const auth = getAuth(firebaseApp);
-// export const db = getFirestore(firebaseApp);
-
-// Firestore initial branch commit
-// Firestore branch second commit
+export const db = getFirestore(firebaseApp);
