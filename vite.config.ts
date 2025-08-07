b07cfbc2f9a41f88cb8df1cdc13bf7efde35273a
Chaosborne <rick.chaosborne@gmail.com>
2025-08-08 00:00:18 +0300

    fix: Vite config base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/shop/',
+  base: '/shop',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
