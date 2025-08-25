6e757778fdc923dbf59d22d93f8bf06af885ff28
Chaosborne <rick.chaosborne@gmail.com>
2025-08-25 18:33:26 +0300

    feat: improve responsive grid layout for products and footer

--- src/components/widgets/Footer/Footer.tsx
+++ src/components/widgets/Footer/Footer.tsx
@@ -1,7 +1,12 @@
 import s from './Footer.module.scss';
+import { useState } from 'react';
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
+
+  const [isTechOpen, setIsTechOpen] = useState(true);
+  const [isFeaturesOpen, setIsFeaturesOpen] = useState(true);
+  const [isContactsOpen, setIsContactsOpen] = useState(true);
 
   return (
     <footer className={s.Footer}>
@@ -15,77 +20,107 @@ const Footer = () => {
           </div>
 
           {/* Технологии */}
-          <div className={s.FooterSection}>
-            <h4 className={s.FooterSubtitle}>Технологии</h4>
-            <div className={s.TechStack}>
-              <div className={s.TechCategory}>
-                <span className={s.TechLabel}>Frontend:</span>
-                <span className={s.TechItems}>React, TypeScript, Redux Toolkit</span>
-              </div>
-              <div className={s.TechCategory}>
-                <span className={s.TechLabel}>Стили:</span>
-                <span className={s.TechItems}>SCSS Modules, CSS Grid, Flexbox</span>
-              </div>
-              <div className={s.TechCategory}>
-                <span className={s.TechLabel}>Backend:</span>
-                <span className={s.TechItems}>Firebase, Firestore, Authentication</span>
-              </div>
-              <div className={s.TechCategory}>
-                <span className={s.TechLabel}>Сборка:</span>
-                <span className={s.TechItems}>Vite, ESLint, Prettier</span>
+          <div className={`${s.FooterSection} ${s.CollapsibleSection} ${isTechOpen ? s.isOpen : ''}`}>
+            <div className={s.CollapsibleHeader}>
+              <h4 className={s.FooterSubtitle}>Технологии</h4>
+              <button
+                type="button"
+                aria-expanded={isTechOpen}
+                className={s.FooterCollapsibleToggler}
+                onClick={() => setIsTechOpen(prev => !prev)}
+              />
+            </div>
+            <div className={s.FooterCollapsibleContent}>
+              <div className={s.TechStack}>
+                <div className={s.TechCategory}>
+                  <span className={s.TechLabel}>Frontend:</span>
+                  <span className={s.TechItems}>React, TypeScript, Redux Toolkit</span>
+                </div>
+                <div className={s.TechCategory}>
+                  <span className={s.TechLabel}>Стили:</span>
+                  <span className={s.TechItems}>SCSS Modules, CSS Grid, Flexbox</span>
+                </div>
+                <div className={s.TechCategory}>
+                  <span className={s.TechLabel}>Backend:</span>
+                  <span className={s.TechItems}>Firebase, Firestore, Authentication</span>
+                </div>
+                <div className={s.TechCategory}>
+                  <span className={s.TechLabel}>Сборка:</span>
+                  <span className={s.TechItems}>Vite, ESLint, Prettier</span>
+                </div>
               </div>
             </div>
           </div>
 
           {/* Функциональность */}
-          <div className={s.FooterSection}>
-            <h4 className={s.FooterSubtitle}>Функциональность</h4>
-            <ul className={s.FeaturesList}>
-              <li>Каталог товаров с фильтрацией</li>
-              <li>Корзина покупок</li>
-              <li>Избранные товары</li>
-              <li>Поиск по товарам</li>
-              <li>Аутентификация пользователей</li>
-              <li>Адаптивный дизайн</li>
-            </ul>
+          <div className={`${s.FooterSection} ${s.CollapsibleSection} ${isFeaturesOpen ? s.isOpen : ''}`}>
+            <div className={s.CollapsibleHeader}>
+              <h4 className={s.FooterSubtitle}>Функциональность</h4>
+              <button
+                type="button"
+                aria-expanded={isFeaturesOpen}
+                className={s.FooterCollapsibleToggler}
+                onClick={() => setIsFeaturesOpen(prev => !prev)}
+              />
+            </div>
+            <div className={s.FooterCollapsibleContent}>
+              <ul className={s.FeaturesList}>
+                <li>Каталог товаров с фильтрацией</li>
+                <li>Корзина покупок</li>
+                <li>Избранные товары</li>
+                <li>Поиск по товарам</li>
+                <li>Аутентификация пользователей</li>
+                <li>Адаптивный дизайн</li>
+              </ul>
+            </div>
           </div>
 
-        {/* Контакты */}
-          <div className={s.FooterSection}>
-            <h4 className={s.FooterSubtitle}>Контакты</h4>
-            <div className={s.Contacts}>
-              <div className={s.ContactItem}>
-                <span className={s.ContactLabel}>GitHub:</span>
-                <a 
-                  href="https://github.com/Chaosborne" 
-                  target="_blank" 
-                  rel="noopener noreferrer"
-                  className={s.ContactLink}
-                >
-                  github.com/Chaosborne
-                </a>
-              </div>
-              <div className={s.ContactItem}>
-                <span className={s.ContactLabel}>Website:</span>
-                <a 
-                  href="https://himmelen.ru" 
-                  target="_blank" 
-                  rel="noopener noreferrer"
-                  className={s.ContactLink}
-                >
-                  himmelen.ru
-                </a>
-              </div>
-              <div className={s.ContactItem}>
-                <span className={s.ContactLabel}>Telegram:</span>
-                <a 
-                  href="https://t.me/Himmelenbot" 
-                  target="_blank" 
-                  rel="noopener noreferrer"
-                  className={s.ContactLink}
-                >
-                  Oleg
-                </a>
+          {/* Контакты */}
+          <div className={`${s.FooterSection} ${s.CollapsibleSection} ${isContactsOpen ? s.isOpen : ''}`}>
+            <div className={s.CollapsibleHeader}>
+              <h4 className={s.FooterSubtitle}>Контакты</h4>
+              <button
+                type="button"
+                aria-expanded={isContactsOpen}
+                className={s.FooterCollapsibleToggler}
+                onClick={() => setIsContactsOpen(prev => !prev)}
+              />
+            </div>
+            <div className={s.FooterCollapsibleContent}>
+              <div className={s.Contacts}>
+                <div className={s.ContactItem}>
+                  <span className={s.ContactLabel}>GitHub:</span>
+                  <a 
+                    href="https://github.com/Chaosborne" 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                    className={s.ContactLink}
+                  >
+                    github.com/Chaosborne
+                  </a>
+                </div>
+                <div className={s.ContactItem}>
+                  <span className={s.ContactLabel}>Website:</span>
+                  <a 
+                    href="https://himmelen.ru" 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                    className={s.ContactLink}
+                  >
+                    himmelen.ru
+                  </a>
+                </div>
+                <div className={s.ContactItem}>
+                  <span className={s.ContactLabel}>Telegram:</span>
+                  <a 
+                    href="https://t.me/Himmelenbot" 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                    className={s.ContactLink}
+                  >
+                    Oleg
+                  </a>
+                </div>
               </div>
             </div>
           </div>
